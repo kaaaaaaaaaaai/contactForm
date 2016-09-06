43ccd29dd62f082c5e98aff785303cc0af74ae3c
kai ogita <kai-ogita@rich.co.jp>
2016-09-06 18:05:05 +0900

    add view

---
+++ resources/views/mail/phrase.blade.php
@@ -0,0 +1,7 @@
+@yield('header')
+---------------------------<br />
+name:{{$data['name']}}<br />
+email:{{$data['email']}}<br />
+性別:{{$data['gender']}}<br />
+comment:{{$data['comment']}}<br />
+---------------------------<br />
---
+++ resources/views/mail/recipient.blade.php
@@ -0,0 +1,5 @@
+@extends('mail.phrase')
+
+@section('header')
+    問い合わせがあります。内容は<br />
+@endsection
